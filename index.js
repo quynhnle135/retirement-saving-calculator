8a93148d61743f5082d0c75217188e978205f77c
Quinn Le <quynhnle135@gmail.com>
2024-02-19 20:33:13 -0500

    Updated user input and monthly contribution

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-function calculateSavingByYears(p, r, t) {
-    r = r / 100;
-    return p * Math.pow((1 + r), t);
+// function calculateSavingByYears(p, r, t) {
+//     r = r / 100;
+//     return p * Math.pow((1 + r), t);
+// }
+
+function calculateSavingByMontlyContribution(p, r, t) {
+    r = (r / 100) / 12;
+    return p * ((Math.pow((1 + r), 12 * t) - 1) / r);
 }
 
 function calculateSaving() {
-    const p = parseFloat(document.getElementById('deposit-amount').value);
+    const p = parseFloat(document.getElementById('contribution').value);
     const r = parseFloat(document.getElementById('interest-rate').value);
-    const t = parseInt(document.getElementById('years').value);
-    let result = calculateSavingByYears(p, r, t);
+    const curAge = parseInt(document.getElementById('current-age').value);
+    const retirementAge = parseInt(document.getElementById('retirement-age').value);
+    const t = retirementAge - curAge;
+
+    let result = calculateSavingByMontlyContribution(p, r, t);
     
     document.getElementById('result').innerText = `Future Value: $${result.toFixed(2)}`;
 }
